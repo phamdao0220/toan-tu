6985b5231d8b3319cbe090badca589515c1abcc1
phamdao0220 <phamd1411200220@gmail.com>
2020-09-10 15:26:57 +0700

    toan tu

---
+++ toantu1.js
@@ -0,0 +1,7 @@
+let inputwidth = prompt("Enter the Width");
+let inputHeight = prompt("Enter the height");
+let width = parseInt(inputwidth);
+let height = parseInt(inputHeight);
+let area =width * height;
+document.write("the area is: " + area);
+
